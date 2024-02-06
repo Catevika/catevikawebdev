67064566605c0de5e520a8d2fe166158a567c232
Catevika <dominique.bello@outlook.fr>
2024-02-06 19:38:58 +0100

    Add not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { barlowCond } from './font';
+import styles from "./page.module.css";
+
+export default function notFound() {
+  return (
+    <main className='container'>
+      <div className={styles.homeBackground}>
+        <Image src={'/images/404.png'} alt='' height={400} width={400} style={{ borderRadius: '50%' }} priority />
+        <div className={styles.notFound}>
+          <h2 className={barlowCond.className}>404 - Page not found</h2>
+          <Link className={styles.notFound__link} href="/">Back to Home page</Link>
+        </div>
+      </div>
+    </main>
+  );
+}
