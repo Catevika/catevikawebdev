df37dea34e178dc4d7b1ec9ea94c4bf02a154221
Catevika <dominique.bello@outlook.fr>
2024-02-07 11:51:12 +0100

    Fix Links component name

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import { barlowCond } from '@/app/font';
 import Link from 'next/link';
-import Links from './links/links';
+import Links from './Links/Links';
 import styles from './navbar.module.css';
 
 export default function Navbar() {
