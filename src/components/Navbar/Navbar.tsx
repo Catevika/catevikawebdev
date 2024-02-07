f46656bf1b1f9a3121ccaa691e663eef9a7f4bc5
Catevika <dominique.bello@outlook.fr>
2024-02-07 19:53:10 +0100

    Update CSS for body, container & navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,11 +5,11 @@ import styles from './navbar.module.css';
 
 export default function Navbar() {
   return (
-    <div className={styles.navbar__container}>
+    <header className={styles.navbar__container}>
       <div className={barlow.className}>
         <Link className={styles.navbar__logo} href={'/'}><span>Catevika Web Dev</span></Link>
       </div>
       <Links />
-    </div>
+    </header>
   );
 }
