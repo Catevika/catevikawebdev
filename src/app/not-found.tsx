c0d966b4d99ae7716bd7f7c632f32d98d3552d02
Catevika <dominique.bello@outlook.fr>
2024-02-14 16:57:20 +0100

    Add not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,16 +1,16 @@
+import { lato } from '@/app/font';
 import Image from 'next/image';
 import Link from 'next/link';
-import { barlow } from './font';
 import styles from "./page.module.css";
 
 export default function notFound() {
   return (
     <main className='container'>
-      <div className={styles.homeBackground}>
+      <div className={styles.home__background}>
         <Image src={'/images/404.png'} alt='' height={400} width={400} style={{ borderRadius: '50%' }} priority />
-        <div className={styles.notFound}>
-          <h2 className={barlow.className}>404 - Page not found</h2>
-          <Link className={styles.notFound__link} href="/">Back to Home page</Link>
+        <div className={styles.not__found}>
+          <h3 className={lato.className}>404 - Page not found</h3>
+          <Link className={styles.not__found__link} href="/">Back to Home page</Link>
         </div>
       </div>
     </main>
