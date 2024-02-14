141020a3133172c3c8a39f9ca592387a0b5c5de6
Catevika <dominique.bello@outlook.fr>
2024-02-14 17:01:40 +0100

    Add Site_under_construction to pending pages

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,12 @@
+import Image from 'next/image';
 
 export default function Contact() {
   return (
-    <main className='container'>Contact</main>
+    <main className='container'>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+        <Image src={'/images/Site_under_construction.png'} alt='' height={504} width={896} priority />
+      </div>
+      <h3>Contact</h3>
+    </main>
   );
 }
