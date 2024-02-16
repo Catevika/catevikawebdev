0d763d7a92512330caecc76a642054764eb74328
Catevika <dominique.bello@outlook.fr>
2024-02-16 21:30:48 +0100

    Update Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,25 +6,26 @@ export default function Footer() {
   return (
     <div className={styles.footer__container}>
       <div className={styles.footer__content}>
-        <div>
-          <Image className={styles.footer__icon} src={'/icons/address.svg'} alt='Address card icon' sizes='2vw' width={0} height={0} />
-          <div>
-            <p>Catevika Web Dev</p>
-            <p>128 Rue de la Boétie</p>
-            <p>75008 Paris</p>
-            <p>FRANCE</p>
-          </div>
-        </div>
-        <div>
-          <Link className={styles.footer__link} href={'/contact'}>catevikawebdev@outlook.fr</Link>
-        </div>
         <div className={styles.footer__icons}>
-          <Image className={styles.footer__icon} src={'/icons/facebook.svg'} alt='Facebook icon' sizes='2vw' width={0} height={0} />
-          <Image className={styles.footer__icon} src={'/icons/social-x.svg'} alt='X ex-twitter icon' sizes='2vw' width={0} height={0} />
-          <Image className={styles.footer__icon} src={'/icons/instagram.svg'} alt='instagram icon' sizes='4vw' width={0} height={0} />
+          <p className={styles.footer__icon__block}>
+            <Link className={styles.footer__link} href={'/contact'}><Image className={styles.footer__icon} src={'/icons/email.svg'} alt='Email icon' sizes='4vw' width={0} height={0} /></Link>
+          </p>
+          <p className={styles.footer__icon__block}>
+            <Link className={styles.footer__link} href={'/contact'}>
+              <Image className={styles.footer__icon} src={'/icons/facebook.svg'} alt='Facebook icon' sizes='2vw' width={0} height={0} />
+            </Link>
+          </p>
+          <p className={styles.footer__icon__block}>
+            <Link className={styles.footer__link} href={'/contact'}>
+              <Image className={styles.footer__icon} src={'/icons/social-x.svg'} alt='X ex-twitter icon' sizes='2vw' width={0} height={0} />
+            </Link>
+          </p>
         </div>
       </div>
-      <p className={styles.footer__copyright} >© Catevika Web Dev 2024</p>
+      <div className={styles.footer__content}>
+        <p>Catevika Web Dev - 128 Rue de la Boétie - 75008 Paris - FRANCE</p>
+        <p>© Catevika Web Dev 2024</p>
+      </div>
     </div>
   );
 }
