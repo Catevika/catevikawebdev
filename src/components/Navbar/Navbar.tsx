6a0475b0325b67d47f4c6d49e8d013f3cf26f753
Catevika <dominique.bello@outlook.fr>
2024-02-16 21:27:42 +0100

    Add responsiveness to Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,7 +7,7 @@ export default function Navbar() {
   return (
     <header className={styles.navbar__container}>
       <div className={lato.className}>
-        <Link className={styles.navbar__logo} href={'/'}><span>Catevika Web Dev</span></Link>
+        <Link className={styles.navbar__logo} href={'/'}><span>Catevika&nbsp;Web&nbsp;Dev</span></Link>
       </div>
       <Links />
     </header>
