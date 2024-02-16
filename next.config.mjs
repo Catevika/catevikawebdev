ea1076044b757ca4e5c5f62bffa00c335c3c2774
Catevika <dominique.bello@outlook.fr>
2024-02-16 21:33:08 +0100

    Update Contact

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import localesPlugin from '@react-aria/optimize-locales-plugin';
+
+const nextConfig = {
+  i18n: {
+    // These are all the locales you want to support in
+    // your application
+    locales: [ 'en' ],
+    // This is the default locale you want to be used when visiting
+    // a non-locale prefixed path e.g. `/hello`
+    defaultLocale: 'en',
+    // This is a list of locale domains and the default locale they
+    // should handle (these are only required when setting up domain routing)
+    // Note: subdomains must be included in the domain value to be matched e.g. "fr.example.com".
+    domains: [
+      {
+        domain: 'catevikawebdev.com',
+        defaultLocale: 'en',
+        http: true
+      } ]
+  },
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      // Don't include any locale strings in the client JS bundle.
+      config.plugins.push(localesPlugin.webpack({ locales: [] }));
+    }
+    return config;
+  }
+};
 
 export default nextConfig;
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,12 +1,114 @@
-import Image from 'next/image';
+"use client";
+import styles from '@/app/contact/contact.module.css';
+import { Button, FieldError, Form, Input, Label, TextArea, TextField } from 'react-aria-components';
+import { Controller, useForm } from 'react-hook-form';
+
+type ContactData = {
+  name: string;
+  email: string;
+  message: string;
+};
 
 export default function Contact() {
+  const { handleSubmit, control, reset } = useForm({
+    defaultValues: {
+      name: '',
+      email: '',
+      message: ''
+    }
+  });
+
+  const onSubmit = (data: ContactData) => {
+    console.log(data);
+    reset();
+  };
+
   return (
     <main className='container'>
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
-        <Image src={'/images/Site_under_construction.png'} alt='' height={504} width={896} priority />
-      </div>
-      <h3>Contact</h3>
+      <Form className={styles.contact__form} onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          control={control}
+          name="name"
+          rules={{ required: 'Name is required.' }}
+          render={({
+            field: { name, value, onChange, onBlur, ref },
+            fieldState: { invalid, error }
+          }) => (
+            <TextField
+              className={styles.contact__textfield}
+              name={name}
+              type="text"
+              value={value}
+              onChange={onChange}
+              onBlur={onBlur}
+              isRequired
+              validationBehavior="aria"
+              isInvalid={invalid}
+            >
+              <Label className={styles.contact__label}>Name</Label>
+              <Input className={styles.contact__input} placeholder="Jane Smith" ref={ref} autoComplete='name' />
+              <FieldError className={styles.contact__error}>{error?.message}</FieldError>
+            </TextField>
+          )}
+        />
+        <Controller
+          control={control}
+          name="email"
+          rules={{
+            required: 'Email is required.',
+            pattern: {
+              value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+              message: 'Entered value does not match email format'
+            }
+          }}
+          render={({
+            field: { name, value, onChange, onBlur, ref },
+            fieldState: { error }
+          }) => (
+            <TextField
+              className={styles.contact__textfield}
+              name={name}
+              type="email"
+              value={value}
+              onChange={onChange}
+              onBlur={onBlur}
+              isRequired
+              validationBehavior="aria"
+              isInvalid={!!error}
+            >
+              <Label className={styles.contact__label}>Email</Label>
+              <Input className={styles.contact__input} placeholder="jane.smith@example.com" ref={ref} autoComplete='email' />
+              <FieldError className={styles.contact__error}>{error?.message}</FieldError>
+            </TextField>
+          )}
+        />
+        <Controller
+          control={control}
+          name="message"
+          rules={{ required: 'Message is required.' }}
+          render={({
+            field: { name, value, onChange, onBlur, ref },
+            fieldState: { invalid, error }
+          }) => (
+            <TextField
+              className={styles.contact__textfield}
+              name={name}
+              type="textarea"
+              value={value}
+              onChange={onChange}
+              onBlur={onBlur}
+              isRequired
+              validationBehavior="aria"
+              isInvalid={invalid}
+            >
+              <Label className={styles.contact__label}>Message</Label>
+              <TextArea className={styles.contact__textarea} placeholder="Tell me about your project!" ref={ref} rows={5} />
+              <FieldError className={styles.contact__error}>{error?.message}</FieldError>
+            </TextField>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </Form>
     </main>
   );
 }
