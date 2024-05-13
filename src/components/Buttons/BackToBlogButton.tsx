faed66f295e5b6db94cdf090dec53c23f5015462
Catevika <dominique.bello@outlook.fr>
2024-05-13 19:47:02 +0200

    Fix translation bugs

--- src/components/Buttons/BackToBlogButton.tsx
+++ src/components/Buttons/BackToBlogButton.tsx
@@ -6,7 +6,7 @@ import { useRouter } from 'next/navigation';
 
 
 export default function BacktoBlogButton() {
-  const t = useTranslations('NotFoundPage');
+  const t = useTranslations('BackButton');
 
   const router = useRouter();
 
@@ -15,6 +15,6 @@ export default function BacktoBlogButton() {
   };
 
   return (
-    <button type='button' className={styles.not__found__link} onClick={handleClick}>&larr; {t('text')}</button>
+    <button type='button' className={styles.not__found__link} onClick={handleClick}>&larr; {t('link')}</button>
   );
 }
