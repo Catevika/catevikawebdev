a1ad506b252f39c764cbaa790120151b5245c8e3
Catevika <dominique.bello@outlook.fr>
2026-08-25 18:26:31 +0200

    Update Dave Gray Youtube embed video iframe

--- src/components/Blog/PostVideo/PostVideo.tsx
+++ src/components/Blog/PostVideo/PostVideo.tsx
@@ -3,13 +3,17 @@ import styles from '@/components/Blog/PostVideo/postVideo.module.css';
 export default function PostVideo({ id }: { id: string; }) {
 
   return (
-    <div className={styles.post__video}>
-      <iframe
-        src={`https://www.youtube-nocookie.com/embed/${id}`}
-        title="Build and Deploy a Next.js Blog with Remote MDX Content Files and Nextjs 13" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerPolicy="strict-origin-when-cross-origin"
-        allowFullScreen
-      />
-    </div>
-  );
+		<div className={styles.post__video}>
+			<iframe
+				width="873"
+				height="491"
+				src={`https://www.youtube.com/embed/${id}?list=PL0Zuz27SZ-6Pk-QJIdGd1tGZEzy9RTgtj`}
+				title="Build and Deploy a Next.js Blog with Remote MDX Content Files and Nextjs 13"
+				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+				referrerPolicy="strict-origin-when-cross-origin"
+				allowFullScreen={true}
+			></iframe>
+		</div>
+	);
 }
+
